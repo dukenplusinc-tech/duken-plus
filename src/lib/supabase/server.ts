5ba0a1f87a14251b19be3ea84d9d56838749f18d
Leonid Marchenko <dev.leonid.marchenko@gmail.com>
2024-06-12 17:54:57 +0300

    fix

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,23 +1,23 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
 
-import type {Database} from "@/lib/supabase/types";
+import type { Database } from '@/lib/supabase/types';
 
 export const createClient = (cookieStore?: ReturnType<typeof cookies>) => {
   // set default cookies store
-  const store = cookieStore || cookies()
+  const store = cookieStore || cookies();
 
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY!,
+    process.env.SUPABASE_SERVICE_KEY!,
     {
       cookies: {
         get(name: string) {
-          return store.get(name)?.value
+          return store.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
           try {
-            store.set({ name, value, ...options })
+            store.set({ name, value, ...options });
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -26,7 +26,7 @@ export const createClient = (cookieStore?: ReturnType<typeof cookies>) => {
         },
         remove(name: string, options: CookieOptions) {
           try {
-            store.set({ name, value: '', ...options })
+            store.set({ name, value: '', ...options });
           } catch (error) {
             // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -35,7 +35,5 @@ export const createClient = (cookieStore?: ReturnType<typeof cookies>) => {
         },
       },
     }
-  )
-}
-
-
+  );
+};

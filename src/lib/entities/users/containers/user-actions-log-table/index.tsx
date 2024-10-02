70d3c642b9c2d42531c01f0abcd0ba20cde3172d
Leonid Marchenko <dev.leonid.marchenko@gmail.com>
2024-10-02 12:01:17 +0300

    feat: redesign user logs

--- src/lib/entities/users/containers/user-actions-log-table/index.tsx
+++ src/lib/entities/users/containers/user-actions-log-table/index.tsx
@@ -1,22 +1,69 @@
 'use client';
 
 import type { FC } from 'react';
+import { IonItem, IonLabel, IonList, IonSpinner } from '@ionic/react';
+import { useTranslations } from 'next-intl';
 
 import { useUserActionLogs } from '@/lib/entities/users/hooks/useUserActionLogs';
-import { DataTable } from '@/components/table';
+import { Badge } from '@/components/ui/badge';
+import { PageHeader } from '@/components/ui/page/header';
+import { EmptyScreen } from '@/components/ui/page/screen/empty';
+import { FormatDate } from '@/components/date/format-date';
 
-import { columns } from './columns';
+export const UserActionsLog: FC<{ id: string; full_name: string }> = ({
+  id,
+  full_name,
+}) => {
+  const t = useTranslations('user_logs');
 
-export const UserActionsLog: FC<{ id: string }> = ({ id }) => {
   const { data, count, isLoading, error } = useUserActionLogs(id);
 
   return (
-    <DataTable
-      data={data}
-      count={count!}
-      columns={columns}
-      isLoading={isLoading}
-      error={error}
-    />
+    <div className="flex flex-col h-full">
+      <PageHeader>{t('title', { name: full_name })}</PageHeader>
+
+      {!isLoading && !data.length && (
+        <EmptyScreen>{t('empty_text')}</EmptyScreen>
+      )}
+
+      {isLoading ? (
+        <div className="flex justify-center p-8">
+          <IonSpinner name="dots" />
+        </div>
+      ) : (
+        <IonList>
+          {data.map((item) => (
+            <IonItem key={item.id}>
+              <IonLabel>
+                <div className="flex space-x-2">
+                  <Badge>{item.action}</Badge>
+                </div>
+              </IonLabel>
+              <IonLabel>
+                <div className="flex space-x-2">
+                  <span className="max-w-[250px] truncate font-medium">
+                    {item.entity}
+                  </span>
+                </div>
+              </IonLabel>
+              <IonLabel>
+                <div className="flex space-x-2">
+                  <span className="max-w-[250px] truncate font-medium">
+                    {item.entity_id}
+                  </span>
+                </div>
+              </IonLabel>
+              <IonLabel>
+                <div className="flex space-x-2">
+                  <FormatDate className="font-medium">
+                    {item.timestamp}
+                  </FormatDate>
+                </div>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+      )}
+    </div>
   );
 };

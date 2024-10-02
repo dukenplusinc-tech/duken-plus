8fa4e8984856399c39fe65a320c7bc757d9062ba
Leonid Marchenko <dev.leonid.marchenko@gmail.com>
2024-10-02 16:27:13 +0300

    improve notes

--- src/lib/entities/notes/containers/notes-table/item.tsx
+++ src/lib/entities/notes/containers/notes-table/item.tsx
@@ -1,9 +1,11 @@
-import { FC, ReactNode } from 'react';
+import { FC } from 'react';
+import Link from 'next/link';
 import { IonButton, IonIcon, IonItem, IonLabel } from '@ionic/react';
 import { ellipsisVertical } from 'ionicons/icons';
 
 import { useNoteDotMenu } from '@/lib/entities/notes/containers/notes-table/dot-menu';
 import type { Note } from '@/lib/entities/notes/schema';
+import * as fromUrl from '@/lib/url/generator';
 import { DropdownButton } from '@/components/ui/ionic/dropdown';
 
 interface NoteItemProps {
@@ -14,16 +16,18 @@ export const NoteItem: FC<NoteItemProps> = ({ note }) => {
   const options = useNoteDotMenu(note);
 
   return (
-    <IonItem key={note.id}>
-      <IonLabel>{note.title || '---'}</IonLabel>
-      <DropdownButton
-        button={
-          <IonButton fill="clear">
-            <IonIcon slot="icon-only" icon={ellipsisVertical} />
-          </IonButton>
-        }
-        options={options}
-      />
-    </IonItem>
+    <Link href={fromUrl.toNoteEdit(note.id)}>
+      <IonItem detail button key={note.id}>
+        <IonLabel>{note.title || '---'}</IonLabel>
+        <DropdownButton
+          button={
+            <IonButton fill="clear">
+              <IonIcon slot="icon-only" icon={ellipsisVertical} />
+            </IonButton>
+          }
+          options={options}
+        />
+      </IonItem>
+    </Link>
   );
 };

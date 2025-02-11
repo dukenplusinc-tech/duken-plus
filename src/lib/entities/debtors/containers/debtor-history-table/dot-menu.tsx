446b008b44db7536a958916f2a04b012a33a6424
Leonid Marchenko <dev.leonid.marchenko@gmail.com>
2025-02-11 10:07:13 +0100

    not allow to delete history records for employee

--- src/lib/entities/debtors/containers/debtor-history-table/dot-menu.tsx
+++ src/lib/entities/debtors/containers/debtor-history-table/dot-menu.tsx
@@ -4,6 +4,7 @@ import { useTranslations } from 'next-intl';
 import { useTransactionForm } from '@/lib/entities/debtors/containers/transaction-form';
 import { useDeleteDebtorTransaction } from '@/lib/entities/debtors/hooks/useDeleteDebtorTransaction';
 import type { DebtorTransaction } from '@/lib/entities/debtors/schema';
+import { useEmployeeMode } from '@/lib/entities/employees/context';
 import type { DropDownButtonOption } from '@/components/ui/ionic/dropdown';
 
 export function useDebtorTransactionDotMenu(
@@ -14,18 +15,21 @@ export function useDebtorTransactionDotMenu(
 
   const handleRemove = useDeleteDebtorTransaction(transaction.id);
 
+  const { isEmployee } = useEmployeeMode();
+
   return useMemo(
-    () => [
-      {
-        label: t('datatable.actions.edit_caption'),
-        onClick: handleEdit,
-      },
-      {
-        label: t('datatable.actions.delete_cation'),
-        onClick: handleRemove.onDelete,
-        disabled: handleRemove.processing,
-      },
-    ],
-    [t, handleEdit, handleRemove.onDelete, handleRemove.processing]
+    () =>
+      [
+        {
+          label: t('datatable.actions.edit_caption'),
+          onClick: handleEdit,
+        },
+        !isEmployee && {
+          label: t('datatable.actions.delete_cation'),
+          onClick: handleRemove.onDelete,
+          disabled: handleRemove.processing,
+        },
+      ].filter(Boolean) as DropDownButtonOption[],
+    [t, handleEdit, isEmployee, handleRemove.onDelete, handleRemove.processing]
   );
 }

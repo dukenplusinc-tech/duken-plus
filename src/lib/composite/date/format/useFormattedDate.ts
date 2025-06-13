77b6c022d469adb41b0abc936e24ed28569f73a2
Leonid Marchenko <dev.leonid.marchenko@gmail.com>
2025-06-13 19:24:19 +0200

    fix date displaying

--- src/lib/composite/date/format/useFormattedDate.ts
+++ src/lib/composite/date/format/useFormattedDate.ts
@@ -1,25 +1,31 @@
 import { useEffect, useState } from 'react';
 import { format, parseISO } from 'date-fns';
+import { Locale as DateFnsLocale, enUS, ru } from 'date-fns/locale';
+import { useLocale } from 'next-intl';
 
-/**
- * Hook to format a datetime string into a human-readable format
- *
- * @param datetime
- * @param dateFormat
- */
+import { Locale } from '@/config/languages';
+
+const localeMap: Record<Locale, DateFnsLocale> = {
+  en: enUS,
+  ru: ru,
+  kz: ru,
+};
+
 export const useFormattedDate = (
   datetime: string | null,
   dateFormat = 'PPpp'
 ) => {
   const [formattedDate, setFormattedDate] = useState('');
+  const localeCode = useLocale();
+  const locale = localeMap[localeCode as Locale] || enUS;
 
   useEffect(() => {
     if (datetime) {
       const date = parseISO(datetime);
-      const formatted = format(date, dateFormat);
+      const formatted = format(date, dateFormat, { locale });
       setFormattedDate(formatted);
     }
-  }, [datetime, dateFormat]);
+  }, [datetime, dateFormat, locale]);
 
   return formattedDate;
 };

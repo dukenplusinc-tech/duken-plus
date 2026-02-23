e417878101974c6a3fe6294ab2bdab8a80ed4097
Leonid Marchenko <dev.leonid.marchenko@gmail.com>
2026-02-23 21:48:55 +0100

    fix cronjob

---
+++ supabase/migrations/20260223000000_schedule_auto_close_cash_shifts.sql
@@ -0,0 +1,14 @@
+-- Schedule auto_close_cash_shifts to run every 15 minutes
+do $$
+begin
+  if not exists (
+    select 1 from cron.job where jobname = 'auto-close-cash-shifts'
+  ) then
+    perform cron.schedule(
+      'auto-close-cash-shifts',
+      '*/15 * * * *',
+      'select public.auto_close_cash_shifts();'
+    );
+  end if;
+end;
+$$;
